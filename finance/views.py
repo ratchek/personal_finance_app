b3fcf82d399a7341972406952af3e068652035fa
ratchek <git@ratchek.com>
2023-04-07 10:48:44 -0400

    Testing deployment script

--- finance/views.py
+++ finance/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+
+# from django.shortcuts import render
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the finance index.")
+    return HttpResponse(
+        "Hello, world. You're at the finance index.\n Don't mind me, just testing da deployment"
+    )
